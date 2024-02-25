1722ff1dd58f85610369c0d6e1fc2588cec85780
ArsalanAyaz <henrykoff7788@gmail.com>
2024-02-25 08:59:56 +0500

    day_3

--- components/NewArrival.tsx
+++ components/NewArrival.tsx
@@ -1,11 +1,14 @@
-"use state"
+"use client"
 import React from 'react';
 import { Whisper } from 'next/font/google';
 import { Data } from '@/utils/productData';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import ProductCard from './ProductCard';
+import { IProduct } from './ProductCard';
 
-const MyWhisper = Whisper({ subsets: ['latin'], weight: ['400'] });
+
+const whisper = Whisper({ subsets: ['latin'], weight: ['400'] });
 
 const NewArrival = () => {
 
@@ -47,8 +50,44 @@ const handleTab = (index: number) => {
   return (
     <div className='container pt-32'>
       <div className="text-center">
-        <h3></h3>
-      </div>
+        <h3 className={`${whisper.className} text-[40px] text-gray-500 `}>
+          For Your Beauty
+        </h3>
+        <h2 className='font-semibold text-5xl'>New Arrival</h2>
+        <ul className='flex flex-col sm:flex-row gap-4 sm:gap-8 justify-center pt-8 uppercase
+        font-medium text-xl '>
+          {tabsData.map((text, index) => (
+            <li 
+            key={text}
+            className={`${
+              selectedTab === index && "text-accent"
+            } cursor-pointer hover:text-accent`}
+            onClick={() => handleTab(index)}
+            >
+              {text}
+            </li>
+          )) }
+        </ul>
+
+
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 pt-8'></div>
+
+            {data?.map((item:IProduct, index: number) => (
+
+              <ProductCard
+              key={item.id}
+              id={item.id}
+              img={item.img}
+              name={item.name}
+              price={item.price}
+              sale={item.sale}
+              category={item.category}
+              />
+            ))};
+
+
+
+            </div>
     </div>
   );
 };
